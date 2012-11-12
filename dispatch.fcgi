040b216ce90be6ba24dcab0ec95f82d05822b267
James Hobin <hobinjk@mit.edu>
2012-11-11 23:12:54 -0800

    Initial commit

---
+++ dispatch.fcgi
@@ -0,0 +1,6 @@
+#!/bin/sh
+export NODE_PATH=node_modules
+echo `dirname "$0"`
+cd `dirname "$0"`
+ls
+exec ./fcgi-shim node server.js 1>&2
---
+++ server.js
@@ -0,0 +1,6 @@
+http.createServer(function (req, res) {
+	req.on('end', function () {
+		res.writeHead(200);
+		res.end();
+	});
+}).listen(process.argv[2]);
